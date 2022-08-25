78d49f6ab91f5c78d23875966c8b1cc8b85ff0e9
Bhargav Shah <bhargav3shah@gmail.com>
2022-08-25 14:51:10 +0530

    Changed server data structure

--- src/functional-components/App.js
+++ src/functional-components/App.js
@@ -1,72 +1,72 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import {SelectProcessor} from './components/ComponentSelectors';
-import Summary from './components/Summary';
-import Price from './components/Price';
+import { SelectProcessor } from "./components/ComponentSelectors";
+import Summary from "./components/Summary";
+import Price from "./components/Price";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [configurableComponents, setConfigurableComponents] = useState({});
+  const [processorList, setProcessorList] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const getMacComponents = () => {
       return fetch(`http://localhost:3004/components`)
-        .then(response => response.json())
-        .then(data => {
-          const newData = {};
-          const configurableComponents = Object.keys(data);
-          for (let i = 0; i < configurableComponents.length; i++) {
-            const component = [...data[configurableComponents[i]]];
-            for (let j = 0; j < component.length; j++) {
-              const element = {...component[j]};
-              element.selected = element.default;
-              component[j] = element;
+        .then((response) => response.json())
+        .then((data) => {
+          const newData = [];
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].type === "proe") {
+              if (data[i].default === true) {
+                data[i].selected = true;
+              } else {
+                data[i].selected = false;
+              }
+              newData.push(data[i]);
             }
-            newData[configurableComponents[i]] = component;
           }
           return newData;
         })
         .catch(() => {
-          throw new Error('could not fetch the customisable components');
+          throw new Error("could not fetch the customisable components");
         });
-    }
+    };
     setLoading(true);
     getMacComponents()
       .then((data) => {
-        setConfigurableComponents(data);
+        setProcessorList(data);
       })
       .catch((error) => {
         setError(error);
       })
       .finally(() => {
         setLoading(false);
-      })
+      });
   }, []);
 
   const setSelectedVariant = (variantSerialNo) => {
-    setConfigurableComponents({
-      ...configurableComponents,
-      Processor: configurableComponents.Processor.map(variant => {
+    setProcessorList(
+      processorList.map((variant) => {
         return {
           ...variant,
-          selected: variant.serialNo === variantSerialNo
-        }
+          selected: variant.serialNo === variantSerialNo,
+        };
       })
-    });
-  }
+    );
+  };
 
   const getAddOnPrice = () => {
-    return Object.keys(configurableComponents).reduce((totalAddOnPrice, component) => {
-      return totalAddOnPrice + configurableComponents[component].find(variant => variant.selected).addOnPrice
-    }, 0);
-  }
+    return processorList.find((variant) => variant.selected)?.addOnPrice ?? 0;
+  };
 
   return (
     <>
       <header>
         <div className="header__content">
-          <a className="header__link" href="https://www.apple.com/in/macbook-pro">
+          <a
+            className="header__link"
+            href="https://www.apple.com/in/macbook-pro"
+          >
             <strong>MacBook Pro</strong>
           </a>
         </div>
@@ -75,22 +75,29 @@ const App = () => {
         <div className="main__container">
           <div className="main__content">
             <section>
-              <img className="macbook-img" alt="macbook pro" src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16touch-space-select-201911?wid=1808&hei=1686&fmt=jpeg&qlt=80&.v=1572825197207" />
+              <img
+                className="macbook-img"
+                alt="macbook pro"
+                src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp16touch-space-select-201911?wid=1808&hei=1686&fmt=jpeg&qlt=80&.v=1572825197207"
+              />
             </section>
             <section className="configuration">
-              {
-                loading ?
-                  <h1>loading...</h1>
-                  :
-                  error ?
-                    <h1>Something went wrong. Please try again later</h1>
-                    :
-                    <>
-                      <h1 className="mt-0">Customise your 16‑inch MacBook Pro - Space Grey</h1>
-                      <Summary configurableComponents={configurableComponents} />
-                      <SelectProcessor variants={configurableComponents.Processor} onSelectVariant={setSelectedVariant} />
-                    </>
-              }
+              {loading ? (
+                <h1>loading...</h1>
+              ) : error ? (
+                <h1>Something went wrong. Please try again later</h1>
+              ) : (
+                <>
+                  <h1 className="mt-0">
+                    Customise your 16‑inch MacBook Pro - Space Grey
+                  </h1>
+                  <Summary processorList={processorList} />
+                  <SelectProcessor
+                    variants={processorList}
+                    onSelectVariant={setSelectedVariant}
+                  />
+                </>
+              )}
             </section>
           </div>
         </div>
@@ -98,6 +105,6 @@ const App = () => {
       </main>
     </>
   );
-}
+};
 
 export default App;
--- src/functional-components/components/Summary.js
+++ src/functional-components/components/Summary.js
@@ -1,13 +1,9 @@
 import React from "react";
 
-const Summary = ({ configurableComponents }) => {
+const Summary = ({ processorList }) => {
   return (
     <ul className="summary-list">
-      {
-        Object.keys(configurableComponents).map((component) => (
-          <li key={`${component}_${component.serialNo}`}>{configurableComponents[component].find(c => c.selected).variant}</li>
-        ))
-      }
+      <li>{processorList.find(c => c.selected)?.variant}</li>
       <li>16-inch Retina display with True Tone</li>
       <li>Four Thunderbolt 3 ports</li>
       <li>Touch Bar and Touch ID</li>
